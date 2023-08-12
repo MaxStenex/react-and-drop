510c3f93d0d7f56c89c74d882bf0621ede88bbfe
Maxim <yourstenex@gmail.com>
2023-08-12 21:47:55 +0300

    cards example layout init

---
+++ src/utils/cards.ts
@@ -0,0 +1,29 @@
+import { Card } from "@/types/card";
+
+export const cardsData: Card[] = [
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, optio.",
+    imageSrc: "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, optio.",
+    imageSrc: "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, optio.",
+    imageSrc: "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+  {
+    id: 4,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, optio.",
+    imageSrc: "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+  {
+    id: 5,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, optio.",
+    imageSrc: "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+];
